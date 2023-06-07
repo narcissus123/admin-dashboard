f93c0839a2ced95c21dde683fd8c2972cd803b3e
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 09:27:48 -0400

    main component.

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client";
 import { App } from "./app/App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./utils/auth/Auth";
+import { ChatProvider } from "./utils/chatContext/ChatContext";
+import { DashboardProvider } from "./utils/dashboardData/DashboardData";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ChatProvider>
+        <DashboardProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </DashboardProvider>
+      </ChatProvider>
     </AuthProvider>
   </React.StrictMode>
 );
