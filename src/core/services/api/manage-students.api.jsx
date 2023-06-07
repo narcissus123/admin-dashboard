3c67906b36a31bec2e5536ebbed593e6a71f0da4
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 09:39:14 -0400

    Component containing REST APIs for managing students.

--- src/core/services/api/manage-students.api.jsx
+++ src/core/services/api/manage-students.api.jsx
@@ -1,10 +1,50 @@
 import { instance } from "../interceptor/Interceptor";
 
+export const createStudent = async (obj) => {
+  try {
+    const response = await instance.post("/api/auth/register", obj);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
 // Getting all students
 export const getStudents = async () => {
   try {
     const response = await instance.get("/api/student/getall");
     return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const deactivateStudentById = async (studentId) => {
+  try {
+    const response = await instance.put(`/api/student/deactive/${studentId}`);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const activateStudentById = async (studentId) => {
+  try {
+    const response = await instance.put(`/api/student/active/${studentId}`);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const updateStudentInfoById = async (studentId, studentInfo) => {
+  try {
+    const response = await instance.put(
+      `api/student/${studentId}`,
+      studentInfo
+    );
+
+    return response;
   } catch (error) {
     return error;
   }
@@ -35,6 +75,15 @@ export const deActiveStudent = async (studentId) => {
   try {
     const response = await instance.get(`/api/student/deactive/${studentId}`);
     return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const deleteStudenById = async (studentId) => {
+  try {
+    const response = await instance.delete(`/api/student/${studentId}`);
+    return response;
   } catch (error) {
     return error;
   }
