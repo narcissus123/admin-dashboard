79a911f4563841e7a46de0659e645c791e0b182e
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-06 22:03:18 -0400

    Added calendar page for users to add events and have their own to do list.

---
+++ src/screens/calendar/Calendar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import CalendarContainer from "../../components/calendarContainer/CalendarContainer";
+export const Calendar = () => {
+  return <CalendarContainer />;
+};
