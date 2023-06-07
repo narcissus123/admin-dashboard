39928fcd5bff1b7298ec17678ab8c2452925e34b
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 09:57:17 -0400

    Layout for users who are not registered yet.

--- src/components/layout/UnAuthLayout.jsx
+++ src/components/layout/UnAuthLayout.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { Box } from "@mui/material";
+import { Outlet, Navigate } from "react-router-dom";
+
 import { SidebarSection } from "./sidebar/Sidebar";
 import { Footer } from "./footer/Footer";
 import { HomeHeader } from "./header/homeHeader/HomeHeader";
 import { Header } from "./header/authHeader/Header";
-import { Box } from "@mui/material";
-import { Outlet, Navigate, useLocation } from "react-router-dom";
+
 import { getItem } from "../../core/services/storage/Storage";
 
 export const UnAuthLayout = ({ sidebarSection, homeHeader, footer }) => {
   const token = Boolean(getItem("employee"));
-  // console.log("token un: ", token);
+
   return token ? (
     <Navigate to="/dashboard" replace />
   ) : (
