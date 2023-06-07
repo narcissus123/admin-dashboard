ebb3779094ad6aecdd503d03ad3cb85e8d7c9ef9
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 09:40:12 -0400

    Component containing REST APIs for user authentication.

--- src/core/services/api/Employee-authentication.api.jsx
+++ src/core/services/api/Employee-authentication.api.jsx
@@ -1,7 +1,7 @@
 import { setItem, clearStorage } from "../storage/Storage";
 import { instance } from "../interceptor/Interceptor";
 
-const SignInEmployee = async (employeeInfo) => {
+export const SignInEmployee = async (employeeInfo) => {
   try {
     const response = await instance.post(
       "/api/auth/employee/login",
@@ -14,42 +14,38 @@ const SignInEmployee = async (employeeInfo) => {
     setItem("token", token);
     setItem("employee", employee);
 
-    console.log(token);
-    console.log(employee);
-
     return response.data;
   } catch (error) {
-    console.log("error sign in: ", error);
     return error;
   }
 };
 
-const SignUpEmployee = async (employee) => {
+export const SignUpEmployee = async (employee) => {
   try {
-    console.log("hello:");
     const response = await instance.post(
       "/api/auth/employee/register",
       employee
     );
 
-    console.log(JSON.stringify(response));
-    console.log(JSON.stringify(response.data));
-
     return response.data;
   } catch (error) {
-    console.log("error sign up: ", error);
     return error;
   }
 };
 
-const SignOutEmployee = async () => {
+export const SignOutEmployee = async () => {
   try {
-    console.log("cleared");
     clearStorage();
   } catch (error) {
-    console.log(error);
     return error;
   }
 };
 
-export { SignInEmployee, SignUpEmployee, SignOutEmployee };
+export const getAllTeachers = async () => {
+  try {
+    const response = await instance.get("api/employee/getallteachers");
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
