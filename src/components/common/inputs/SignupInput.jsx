d87629cfebc3ca4165f5f85dbb1a677d9bca6598
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 09:59:12 -0400

    Common input.

--- src/components/common/inputs/SignupInput.jsx
+++ src/components/common/inputs/SignupInput.jsx
@@ -15,6 +15,39 @@ export const Input = ({
 
   return (
     <TextField
+      defaultValue={formik.initialValues[name]}
+      fullWidth={fullWidth}
+      variant={variant}
+      type={type}
+      label={label}
+      name={name}
+      onBlur={formik.handleBlur}
+      value={formik.values[name]}
+      onChange={formik.handleChange}
+      error={formik.touched[name] && !!formik.errors[name]}
+      helperText={formik.touched[name] && formik.errors[name]}
+      sx={sx}
+      required={required}
+      autoComplete={name}
+    />
+  );
+};
+
+export const MutiLineInput = ({
+  variant,
+  type,
+  label,
+  name,
+  sx,
+  fullWidth,
+  required,
+  row,
+}) => {
+  const formik = useFormikContext();
+
+  return (
+    <TextField
+      defaultValue={formik.initialValues[name]}
       fullWidth={fullWidth}
       variant={variant}
       type={type}
@@ -27,6 +60,8 @@ export const Input = ({
       helperText={formik.touched[name] && formik.errors[name]}
       sx={sx}
       required={required}
+      multiline
+      row={row}
     />
   );
 };
