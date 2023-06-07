b14997e6c94a1adbdc7f65c2f38801ffa5595eb5
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-06 22:15:32 -0400

    Reply comment data.

---
+++ src/config/data/manageCommentData/ReplyCommentData.jsx
@@ -0,0 +1,13 @@
+export const ReplyCommentData = [
+  {
+    fullWidth: true,
+    variant: "filled",
+    type: "text",
+    label: "Reply",
+    name: "answer",
+    sx: { gridColumn: "span 4" },
+    multiline: true,
+    row: "5",
+    required: true,
+  },
+];
