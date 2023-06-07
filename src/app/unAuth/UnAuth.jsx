f0315ef226cf50755bc8981c7a1c0b730ea6444c
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 10:06:35 -0400

    component containig routes for unathenticated users.

--- src/app/unAuth/UnAuth.jsx
+++ src/app/unAuth/UnAuth.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { publicRoutes } from "../../config/Routes";
-import { Layout } from "../../components/layout/Layout";
-import { useAuth } from "../../utils/auth/Auth";
 
-export const UnAuth = ({ auth }) => {
-  const auth1 = useAuth();
-  console.log("isEmployee unauth: ", auth.isEmployee);
-  console.log("isEmployee auth1: ", auth1.isEmployee);
+import { publicRoutes } from "../../config/Routes";
+
+export const UnAuth = () => {
   return (
     <Routes>
       {publicRoutes.map((route, index) => (
