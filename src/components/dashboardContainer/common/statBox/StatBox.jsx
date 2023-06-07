a88b9d316d9f009e521bc12a225bb40bf3e28b24
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 09:23:27 -0400

    Common components of dashboard page.

---
+++ src/components/dashboardContainer/common/statBox/StatBox.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../../../global/theme/Theme";
+import PieChart from "../../../common/pieChart/PieChart";
+
+import ProgressCircle from "../../../common/progressCircle/ProgressCircle";
+
+const StatBox = ({
+  title,
+  subtitle,
+  icon,
+  increase,
+  pieStyle,
+  pieOptions,
+  pieData,
+}) => {
+  const theme = useTheme("pieData", pieData);
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box width="100%" m="0 30px">
+      <Box display="flex" justifyContent="space-between">
+        <Box>
+          {icon}
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            sx={{ color: colors.grey[100] }}
+          >
+            {title}
+          </Typography>
+        </Box>
+        <Box>
+          <PieChart
+            pieStyle={pieStyle}
+            pieOptions={pieOptions}
+            pieData={pieData}
+          />
+          {/* <ProgressCircle progress={progress} /> */}
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="space-between" mt="2px">
+        <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+          {subtitle}
+        </Typography>
+        <Typography
+          variant="h5"
+          fontStyle="italic"
+          sx={{ color: colors.greenAccent[600] }}
+        >
+          {increase}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default React.memo(StatBox);
