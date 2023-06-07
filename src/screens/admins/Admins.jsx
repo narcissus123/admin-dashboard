935a59dab5d7e4da3b1e5e97f6841df5f354d7e2
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-06 22:02:06 -0400

    Created page for managing admins.

---
+++ src/screens/admins/Admins.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AdminContainer from "../../components/EmployeeContainer/adminContainer/AdminContainer";
+
+export const Admins = () => {
+  return <AdminContainer />;
+};
