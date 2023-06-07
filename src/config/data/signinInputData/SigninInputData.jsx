fda26c896f491309c478e5de629c6beda7aa36be
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 09:42:12 -0400

    Component containing data for sign in form.

--- src/config/data/signinInputData/SigninInputData.jsx
+++ src/config/data/signinInputData/SigninInputData.jsx
@@ -1,20 +1,20 @@
 export const signinInputData = [
   {
     fullWidth: true,
-    variant: "filled",
+    variant: "outlined",
     type: "text",
-    label: "Email",
+    label: "Email Address",
     name: "email",
-    sx: { paddingBottom: "30px" },
+    sx: { paddingBottom: "30px", margin: "normal" },
     required: true,
   },
   {
     fullWidth: true,
-    variant: "filled",
+    variant: "outlined",
     type: "text",
     label: "Password",
     name: "password",
-    sx: { paddingBottom: "30px" },
+    sx: { paddingBottom: "30px", margin: "normal" },
     required: true,
   },
 ];
